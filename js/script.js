e0608ba5aa086de35c0548a6518289efb4a24e39
Хоменко Олена Володимирівна <Khomenko@khnpp.ua>
2022-12-14 16:44:10 +0200

    add tabs  commit

--- js/script.js
+++ js/script.js
@@ -11,3 +11,16 @@ const slider = tns({
 
 document.querySelector('.prev').addEventListener('click', () => slider.goTo('prev'));
 document.querySelector('.next').addEventListener('click', () => slider.goTo('next'));
+
+
+(function ($) {
+    $(function () {
+
+        $('ul.catalog__tabs').on('click', 'li:not(.catalog__tab_active)', function () {
+            $(this)
+                .addClass('catalog__tab_active').siblings().removeClass('catalog__tab_active')
+                .closest('div.container').find('div.catalog__content').removeClass('catalog__content_active').eq($(this).index()).addClass('catalog__content_active');
+        });
+
+    });
+})(jQuery);
