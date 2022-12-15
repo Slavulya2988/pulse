570705bdfe93f29bb8d2d1ca3cfcb96914358bca
SlavaUA <slavaUA@gmail.com>
2022-12-15 15:43:23 +0200

    add animation for card on JS

--- js/script.js
+++ js/script.js
@@ -26,18 +26,41 @@ document.querySelector('.next').addEventListener('click', () => slider.goTo('nex
 
 })(jQuery);
 
-function toggleSlide(item) {
-    $(item).each(function (i) {
-        $(this).on('click', function (e) {
+
+function toggleSlideJS(itemsClass) {
+    const
+        links = document.querySelectorAll(itemsClass),
+        content = document.querySelectorAll('.catalog-item__content'),
+        list = document.querySelectorAll('.catalog-item__list');
+
+
+    links.forEach((link, i) => {
+        link.addEventListener('click', (e) => {
             e.preventDefault();
-
-            $('.catalog-item__content').eq(i).toggleClass('catalog-item__content_active');
-            $('.catalog-item__list').eq(i).toggleClass('catalog-item__list_active');
-
-
+            if (e.target == link) {
+                content[i].classList.toggle('catalog-item__content_active');
+                list[i].classList.toggle('catalog-item__list_active');
+            }
         });
     });
-}
 
-toggleSlide('.catalog-item__link');
-toggleSlide('.catalog-item__back');
+
+}
+toggleSlideJS('.catalog-item__link');
+toggleSlideJS('.catalog-item__back');
+
+// function toggleSlide(item) {
+//     $(item).each(function (i) {
+//         $(this).on('click', function (e) {
+//             e.preventDefault();
+
+//             $('.catalog-item__content').eq(i).toggleClass('catalog-item__content_active');
+//             $('.catalog-item__list').eq(i).toggleClass('catalog-item__list_active');
+
+
+//         });
+//     });
+// }
+
+// toggleSlide('.catalog-item__link');
+// toggleSlide('.catalog-item__back');
