20aeff52d5285ed6cfae57bbdb11f87c743f9a04
Хоменко Олена Володимирівна <Khomenko@khnpp.ua>
2022-12-15 13:18:54 +0200

    add animation for card

--- js/script.js
+++ js/script.js
@@ -23,4 +23,21 @@ document.querySelector('.next').addEventListener('click', () => slider.goTo('nex
         });
 
     });
+
 })(jQuery);
+
+function toggleSlide(item) {
+    $(item).each(function (i) {
+        $(this).on('click', function (e) {
+            e.preventDefault();
+
+            $('.catalog-item__content').eq(i).toggleClass('catalog-item__content_active');
+            $('.catalog-item__list').eq(i).toggleClass('catalog-item__list_active');
+
+
+        });
+    });
+}
+
+toggleSlide('.catalog-item__link');
+toggleSlide('.catalog-item__back');
